343ffc3a3a42e61b50ac53dcf36fff6055203351
John Stassen <john.stassen@gmail.com>
2020-08-29 15:38:51 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,14 @@
 class Appointment
-  
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
